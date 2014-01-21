4013e5547a6a6c83573af820128d0780ac524b25
Dan Reisman <wrstone@etrigan.localhost>
2014-01-21 16:28:52 -0500

    Start of unit test framework, creation of mock job objects and mock jobqueue

---
+++ mockjob.py
@@ -0,0 +1,37 @@
+from jobqueue import Job
+
+class MockJob(Job):
+
+   # Only the init function needs reimplmentation, the __cmp__  /
+   # __str__  /  __repr__ methods  should be allowed to be inherited from
+   # the actual Job class; note that  not all fields are reproducted, just
+   # those used by the inherieted methods
+   def __init__(self, jobId, uuid, sha512, username, creation, pagecount, client, title, duplex=False, state=3):
+        self.__hash__ = lambda: jobId
+        self.jobId = jobId
+	self.printerUri = printerUri
+	self.uuid = uuid
+	self.sha512 = sha512
+	self.username = username
+	self.creation = creation
+	self.pagecount = pagecount
+	self.client = client
+	self.title = title
+	self.duplex = duplex
+	self.jobState = state
+	self.displayTitle = self.title[:47]
+
+class MockQueue(JobQueue):
+
+   def __init__(self, unipattern=None, conn=None, multicastHandler=None):
+       if unipattern is not None:
+          self.unipattern = unipattern
+       else:
+          self.unipattern = re.compile('.*')
+       self.conn = None
+       self.mcast = None
+
+
+   def refresh:
+       print "refresh called"
+
