45f577bd5689fea70227136bb3121ac1bf463ee8
Dan Reisman <dr2481@columbia.edu>
2014-09-05 09:36:45 -0400

    Added shorcuts around sftp for previous sent files as well as localhost destinations

--- sftp_push.sh
+++ sftp_push.sh
@@ -13,17 +13,34 @@ fork_sftp() {
       exit 1
    fi
 
+   if [ -f "/svc/remote/UPLOAD/${username}_${sha512}" ]; then
+      echo $(date +'%s') $username $filename $sha512 has been previously processed >> $log
+      exit 0
+   fi
+
+   thishost=$(/bin/cat /etc/mailname)
 
    for host in $*; do
-      sftp $host > /dev/null 2>&1 <<-__CMD_SCRIPT__
+
+      if [ $host = $thishost ]; then
+        if [ ! -d /svc/remote/${username} ]; then
+           mkdir /svc/remote/${username}
+        fi
+        cp ${filename} /svc/remote/${username}/${sha512}
+        echo $(date +'%s') $username $filename $sha512 $host $? >> $log
+      else
+        sftp $host > /dev/null 2>&1 <<-__CMD_SCRIPT__
 	cd /svc/remote
 	mkdir ${username}
 	cd ${username}
 	put ${filename} ${sha512}
 	exit 
 __CMD_SCRIPT__
-      echo $(date +'%s') $username $filename $sha512 $host $? >> $log
+        echo $(date +'%s') $username $filename $sha512 $host $? >> $log
+      fi
    done
+
+   /usr/bin/touch /svc/remote/UPLOAD/${username}_${sha512}
 
    if [ -f $filename ]; then
       rm $filename
